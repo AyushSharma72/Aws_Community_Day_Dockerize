f34668239864634e4eff8f8b16b39ecaf873babe
Ayush <asharma7588@gmail.com>
2024-07-10 20:17:07 +0530

    CI CD test

--- src/components/eventhighlights.jsx
+++ src/components/eventhighlights.jsx
@@ -10,7 +10,7 @@ const Eventhighlights = () => {
       <div className=" flex flex-col gap-y-7 justify-around w-full pt-10 pb-10 sm:w-full sm:flex-row sm:pt-20 sm:pb-20  md:w-3/4  ">
         <div className="flex flex-col justify-center items-center gap-y-3">
           <p className="flex justify-center text-3xl font-bold text-[#401f71] leading-9 sm:text-3xl">
-            <CountUp end={400} duration={2} suffix="+" />
+            <CountUp end={500} duration={2} suffix="+" />
           </p>{" "}
           <p className="flex justify-center text-lg font-bold items-center gap-1 sm:text-lg">
             <IoIosPeople className="text-xl sm:text-xl" />
